0e0dc1fde95aad7b21f5b278f569cf57576d8cc5
danielailie <danielailie12@gmail.com>
2021-03-30 14:36:21 +0300

    Add gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'nestjs'],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:nestjs/recommended'
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-use-before-define': 'off',
+    'nestjs/use-dependency-injection': 'error',
+    'nestjs/deprecated-api-modules': 'error',
+    '@typescript-eslint/quotes': ["warn", "single"]
+  },
+};
