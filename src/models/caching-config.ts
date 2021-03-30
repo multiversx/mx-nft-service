4360c4c1b2488fddde1fd022b28d2f7b6e430ba5
danielailie <danielailie12@gmail.com>
2021-03-30 14:57:45 +0300

    Clean up

--- src/models/caching-config.ts
+++ src/models/caching-config.ts
@@ -3,26 +3,6 @@
  */
 export class CachingConfig {
   getAllContractAddresses: number;
-  getContractConfig: number;
-  getBlsKeysStatus: number;
-  getMetaData: CacheWithVerify;
-
-  getUserUnBondable: number;
-  getUserActiveStake: number;
-  getClaimableRewards: number;
-  getUserUnDelegatedList: number;
-
-  getTotalActiveStake: number;
-  getTotalUnStaked: number;
-  getTotalCumulatedRewards: number;
-  getNumUsers: number;
-  getNumNodes: number;
-
-  allProviders: number;
-
-  // keybase
-  verifyIdentity: CacheWithVerify;
-  getProfile: number;
 
   // network config and network status
   networkConfig: number;
