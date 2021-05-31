deea6c1ca302a24f044341800c1d67142270cf8a
danielailie <danielailie12@gmail.com>
2021-05-31 19:13:10 +0300

    clean up

--- src/modules/ipfs/ipfs.service.ts
+++ src/modules/ipfs/ipfs.service.ts
@@ -13,36 +13,41 @@ export class IpfsService {
     const readStream = await fileData.createReadStream();
     const fileType = await this.readStreamToBuffer(readStream);
 
-    const ipfs = await IPFS.create({
-      host: fileStorage.host,
-      port: fileStorage.port,
-      protocol: fileStorage.protocol,
-    });
+    const ipfs = await this.getIpfs();
     const payload = await ipfs.add(fileType);
     const { path } = payload;
-    const url = `${fileStorage.cdnUrl}${path}`;
 
     ipfs.stop();
-    return new UploadToIpfsResult({
-      hash: path,
-      url: url,
-    });
+    return this.mapReturnType(path);
   }
 
   async uploadText(text: any): Promise<UploadToIpfsResult> {
-    const ipfs = await IPFS.create({
+    const ipfs = await this.getIpfs();
+    const payload = await ipfs.add(text);
+    const { path } = payload;
+
+    ipfs.stop();
+    return this.mapReturnType(path);
+  }
+
+  private getUrl(path: any) {
+    return `${fileStorage.cdnUrl}${path}`;
+  }
+
+  private mapReturnType(
+    path: any,
+  ): UploadToIpfsResult | PromiseLike<UploadToIpfsResult> {
+    return new UploadToIpfsResult({
+      hash: path,
+      url: this.getUrl(path),
+    });
+  }
+
+  private async getIpfs() {
+    return await IPFS.create({
       host: fileStorage.host,
       port: fileStorage.port,
       protocol: fileStorage.protocol,
-    });
-    const payload = await ipfs.add(text);
-    const { path } = payload;
-    const url = `${fileStorage.cdnUrl}${path}`;
-
-    ipfs.stop();
-    return new UploadToIpfsResult({
-      hash: path,
-      url: url,
     });
   }
 
