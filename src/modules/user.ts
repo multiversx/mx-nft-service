5c91f2122d3bd051cb9c17f218afc77565db10a0
danielailie <danielailie12@gmail.com>
2021-07-12 15:59:40 +0300

    Add user

---
+++ src/modules/user.ts
@@ -0,0 +1,10 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+export const User = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const gqlCtx = GqlExecutionContext.create(ctx);
+    const request = gqlCtx.getContext().req;
+    return request.user;
+  },
+);
