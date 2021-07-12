257e643c9dbadca695cd2083da59b88fb7ebaa8a
danielailie <danielailie12@gmail.com>
2021-07-12 15:20:59 +0300

    Remove commets

--- src/models/errors/unauthorized-error.ts
+++ src/models/errors/unauthorized-error.ts
@@ -1,13 +1,6 @@
 import { HttpStatus, UnauthorizedException } from '@nestjs/common';
 
-/**
- * Extension class for UnauthorizedException. We add the http status code too.
- */
 export class UnauthorizedError extends UnauthorizedException {
-  /**
-   * Create a new object based on an Error received.
-   * @param message
-   */
   static fromError({ message }: Error) {
     return new UnauthorizedError({
       message,
