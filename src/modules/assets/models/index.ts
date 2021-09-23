72c62d53b31e7341f8717e58d562df6901dd9964
danielailie <danielailie12@gmail.com>
2021-09-23 11:19:22 +0300

    Remove unused class

--- src/modules/assets/models/index.ts
+++ src/modules/assets/models/index.ts
@@ -1,7 +1,6 @@
 export * from './CreateNftArgs';
 export * from './HandleQuantityArgs';
 export * from './TransferNftArgs';
-export * from './CreateAssetArgs';
 export * from './Asset.dto';
 export * from './Attributes.dto';
 export * from './Price.dto';
