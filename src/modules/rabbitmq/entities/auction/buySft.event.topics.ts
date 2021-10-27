be27c2c6997960b82577692ab9de907e21a9ba52
danielailie <danielailie12@gmail.com>
2021-10-27 16:05:35 +0300

    undo change

--- src/modules/rabbitmq/entities/auction/buySft.event.topics.ts
+++ src/modules/rabbitmq/entities/auction/buySft.event.topics.ts
@@ -9,13 +9,14 @@ export class BuySftEventsTopics {
   private boughtTokens: string;
 
   constructor(rawTopics: string[]) {
+    console.log({ rawTopics });
     this.collection = Buffer.from(rawTopics[1], 'base64').toString();
     this.nonce = Buffer.from(rawTopics[2], 'base64').toString('hex');
     this.auctionId = Buffer.from(rawTopics[3], 'base64').toString('hex');
-    this.boughtTokens = Buffer.from(rawTopics[4], 'base64')
-      .toString('hex')
-      .hexBigNumberToString();
-    this.currentWinner = new Address(Buffer.from(rawTopics[5], 'base64'));
+    // this.boughtTokens = Buffer.from(rawTopics[4], 'base64')
+    //   .toString('hex')
+    //   .hexBigNumberToString();
+    this.currentWinner = new Address(Buffer.from(rawTopics[4], 'base64'));
     this.bid = Buffer.from(rawTopics[6], 'base64')
       .toString('hex')
       .hexBigNumberToString();
