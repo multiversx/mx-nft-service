fa4b09f8758281e4dc6628fe50fa636fa046825a
danielailie <danielailie12@gmail.com>
2022-01-27 16:08:44 +0200

    Fix top bider provider

--- src/db/orders/orders.module.db.ts
+++ src/db/orders/orders.module.db.ts
@@ -24,6 +24,7 @@ import { AvailableTokensForAuctionProvider } from './available-tokens-auction.lo
     OrdersServiceDb,
     OrdersProvider,
     LastOrderProvider,
+    LastOrderTopBidProvider,
     AvailableTokensForAuctionProvider,
   ],
 })
