80719ac8a2248784e34884717815676e888be10e
danielailie <danielailie12@gmail.com>
2022-02-10 17:06:18 +0200

    Update packages and delete unused references

--- src/modules/ipfs/ipfs.module.ts
+++ src/modules/ipfs/ipfs.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ElrondCommunicationModule } from '../../common/services/elrond-communication/elrond-communication.module';
-import { IpfsService as IpfsService } from './ipfs.service';
 import { PinataService } from './pinata.service';
 
 @Module({
-  providers: [IpfsService, PinataService],
+  providers: [PinataService],
   imports: [ElrondCommunicationModule],
-  exports: [IpfsService, PinataService],
+  exports: [PinataService],
 })
 export class IpfsModule {}
