d4fdeff7bb0584f72c6c88dfbaab5cf263ad1149
danielailie <danielailie12@gmail.com>
2022-02-10 14:17:51 +0200

    Update db_name

--- ormconfig.js
+++ ormconfig.js
@@ -3,7 +3,7 @@ const db = {
   port: process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
 };
 
 const dbSlaves = process.env.DB_SLAVES
