aab8b8db78fc3222c135bb80bdf67c8264991262
danielailie <danielailie12@gmail.com>
2022-02-17 16:22:49 +0200

    Fix bidding count

--- src/db/account-stats/stats.querries.ts
+++ src/db/account-stats/stats.querries.ts
@@ -7,16 +7,23 @@ export function getAccountStatsQuery(address: string) {
           AND a.status in ('Running', 'Claimable')
       ), 
       
-    ordersCount as (SELECT COUNT(*) orders, SUM(o.priceAmountDenominated) as biddingBalance, ownerAddress  FROM orders o
+    ordersCount as (SELECT COUNT(*) orders, o.ownerAddress
+      FROM orders o
+      INNER JOIN auctions a on a.id=o.auctionId
       WHERE o.ownerAddress = '${address}'
-          AND o.status = 'Active'
-      )
+      AND o.status in ('Active','Inactive') AND a.status='Running'
+      ),
+      biddingBalanceOrders as (SELECT SUM(o.priceAmountDenominated) as biddingBalance, o.ownerAddress as orderAddress
+      from orders o WHERE o.ownerAddress = '${address}'
+            AND o.status ='Active'
+      ) 
   
 SELECT auctions, if(orders, orders,0) as orders, if(biddingBalance, biddingBalance, 0) as biddingBalance, address
 from
   (
   select * from auctionsStats a
   left join ordersCount b on a.address = b.ownerAddress
+  left join biddingBalanceOrders bb on a.address = bb.orderAddress
   ) temp
   `;
 }
