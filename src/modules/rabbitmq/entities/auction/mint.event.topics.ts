5dd41d4189f39a3d2421aebe450e2d3de674fa83
danielailie <danielailie12@gmail.com>
2022-03-02 15:15:33 +0200

    add mint events handle

---
+++ src/modules/rabbitmq/entities/auction/mint.event.topics.ts
@@ -0,0 +1,19 @@
+import { Address } from '@elrondnetwork/erdjs/out';
+import '../../../../utils/extentions';
+
+export class MintEventsTopics {
+  private collection: string;
+  private nonce: string;
+
+  constructor(rawTopics: string[]) {
+    this.collection = Buffer.from(rawTopics[0], 'base64').toString();
+    this.nonce = Buffer.from(rawTopics[1], 'base64').toString('hex');
+  }
+
+  toPlainObject() {
+    return {
+      collection: this.collection,
+      nonce: this.nonce,
+    };
+  }
+}
