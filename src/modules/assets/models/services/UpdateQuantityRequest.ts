ea5d83b2c0280580d8c8ef6538fdc9d2815625bf
danielailie <danielailie12@gmail.com>
2022-03-03 11:13:59 +0200

    Update assets service

---
+++ src/modules/assets/models/services/UpdateQuantityRequest.ts
@@ -0,0 +1,23 @@
+import { HandleQuantityArgs } from '../HandleQuantityArgs';
+
+export class UpdateQuantityRequest {
+  updateQuantityRoleAddress: string;
+  identifier: string;
+  quantity: string;
+  functionName: 'ESDTNFTAddQuantity' | 'ESDTNFTBurn';
+  constructor(init?: Partial<UpdateQuantityRequest>) {
+    Object.assign(this, init);
+  }
+
+  static fromArgs(
+    updateQuantityArgs: HandleQuantityArgs,
+    functionName: 'ESDTNFTAddQuantity' | 'ESDTNFTBurn',
+  ) {
+    return new UpdateQuantityRequest({
+      updateQuantityRoleAddress: updateQuantityArgs.addOrBurnRoleAddress,
+      identifier: updateQuantityArgs.identifier,
+      quantity: updateQuantityArgs.quantity,
+      functionName: functionName,
+    });
+  }
+}
