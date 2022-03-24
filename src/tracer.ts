b9d6936401e7e501cd3e4950435522142ddf114a
danielailie <danielailie12@gmail.com>
2022-03-24 15:27:43 +0200

    remoce gc stats

--- src/tracer.ts
+++ src/tracer.ts
@@ -1,8 +1,3 @@
 import tracer from 'dd-trace';
 tracer.init(); // initialized in a different file to avoid hoisting.
 export default tracer;
-
-var gc = require('gc-stats')();
-gc.on('stats', function (stats) {
-  console.log('GC happened ', stats);
-});
