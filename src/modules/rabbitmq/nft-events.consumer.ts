5f686ee797e4537e5d24d6bf5673f998ce2dc30d
danielailie <danielailie12@gmail.com>
2022-03-24 12:33:15 +0200

    Clean up unused references

--- src/modules/rabbitmq/nft-events.consumer.ts
+++ src/modules/rabbitmq/nft-events.consumer.ts
@@ -1,18 +1,12 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { elrondConfig } from 'src/config';
-import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { NftEventEnum } from '../assets/models/AuctionEvent.enum';
 import { NftEventsService } from './nft-events.service';
 import { CompetingRabbitConsumer } from './rabbitmq.consumers';
-import { ConsumeMessage } from 'amqplib';
-import { Logger } from 'winston';
 
 @Injectable()
 export class NftTransactionsConsumer {
-  constructor(
-    private readonly nftTransactionsService: NftEventsService,
-    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
-  ) {}
+  constructor(private readonly nftTransactionsService: NftEventsService) {}
 
   @CompetingRabbitConsumer({
     queueName: process.env.RABBITMQ_QUEUE,
