8f448306e1c3471d95867952e892909d42ded220
danielailie <danielailie12@gmail.com>
2022-06-02 17:39:53 +0300

    remove console logs

--- src/modules/rabbitmq/minter-events.service.ts
+++ src/modules/rabbitmq/minter-events.service.ts
@@ -11,10 +11,8 @@ export class MinterEventsService {
 
   public async handleNftMinterEvents(mintEvents: any[], hash: string) {
     for (let event of mintEvents) {
-      console.log('for event', event);
       switch (event.identifier) {
         case MinterEventEnum.callBack:
-          console.log('callBack', event);
           const brandEvent = new BrandCreatedEvent(event);
           const topics = brandEvent.getTopics();
           if (topics.eventIdentifier === MinterEventEnum.brandCreated) {
@@ -26,11 +24,8 @@ export class MinterEventsService {
           break;
 
         case MinterEventEnum.buyRandomNft:
-          console.log('buy random', event);
           const randomNftEvent = new RandomNftEvent(event);
           const transferTopics = randomNftEvent.getTopics();
-
-          console.log('data', event.data, randomNftEvent.getData());
 
           const tier = await this.campaignService.updateTier(
             randomNftEvent.getAddress(),
@@ -39,7 +34,6 @@ export class MinterEventsService {
             randomNftEvent.getData(),
           );
           if (tier) {
-            console.log('update tier');
             await this.campaignService.invalidateKey();
           }
 
