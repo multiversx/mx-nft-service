e24f9ef0b645c9d2924795f674b2c315ff0cbf29
danielailie <danielailie12@gmail.com>
2022-06-02 10:07:30 +0300

    Update colection stats query

--- src/db/collection-stats/collection-stats.querries.ts
+++ src/db/collection-stats/collection-stats.querries.ts
@@ -1,26 +1,26 @@
 export function getCollectionStats(identifier: string) {
   return `
   WITH
-    endedAuctions AS (select  SUM(o.priceAmountDenominated) as volumeTraded, 
-      AVG(o.priceAmountDenominated) as saleAverage,
-      MAX(o.priceAmountDenominated) as maxPrice,
-      SUM(if(a.status='Ended', 1, 0)) as auctionsEnded,
-      a.collection as endedIdentifier
-    from orders o 
-    left join auctions a on o.auctionId = a.id 
-    where a.collection  = '${identifier}' and o.status = 'Bought'),
+    endedAuctions AS (select  SUM(o.priceAmountDenominated) AS volumeTraded, 
+      AVG(o.priceAmountDenominated) AS saleAverage,
+      MAX(o.priceAmountDenominated) AS maxPrice,
+      SUM(if(a.status='Ended', 1, 0)) AS auctionsEnded,
+      a.collection AS endedIdentifier
+    FROM orders o 
+    LEFT JOIN auctions a ON o.auctionId = a.id 
+    WHERE a.collection  = '${identifier}' AND o.status = 'Bought'),
 
-    activeAuctions AS (SELECT MIN(if(o.priceAmountDenominated, o.priceAmount , a.minBid)) as minPrice, 
-    COUNT(DISTINCT(a.id)) as activeAuctions,
-    a.collection as activeIdentifier
-    from auctions a
-    left join orders o on o.auctionId = a.id 
-    where a.collection  = '${identifier}' and a.status = 'Running') 
+    activeAuctions AS (SELECT MIN(if(o.priceAmountDenominated, o.priceAmount , a.minBid)) AS minPrice, 
+    COUNT(DISTINCT(a.id)) AS activeAuctions,
+    a.collection AS activeIdentifier
+    FROM auctions a
+    LEFT JOIN orders o ON o.auctionId = a.id 
+    WHERE a.collection  = '${identifier}' AND a.status = 'Running') 
 
     SELECT volumeTraded, saleAverage, maxPrice, auctionsEnded, activeAuctions, minPrice
     FROM (
-    SELECT * from endedAuctions ea
-    LEFT JOIN activeAuctions aa on aa.activeIdentifier = '${identifier}'
+    SELECT * FROM endedAuctions ea
+    LEFT JOIN activeAuctions aa ON aa.activeIdentifier = '${identifier}'
     ) temp
   `;
 }
