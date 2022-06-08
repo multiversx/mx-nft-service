55f63a4cc47e98d38987c5c912fa2606219f2b27
danielailie <danielailie12@gmail.com>
2022-06-08 12:49:59 +0300

    Fix collection stats filter

--- src/modules/collection-stats/models/Collection-Stats.Filter.ts
+++ src/modules/collection-stats/models/Collection-Stats.Filter.ts
@@ -1,11 +1,16 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { Matches } from 'class-validator';
-import { NFT_IDENTIFIER_ERROR, NFT_IDENTIFIER_RGX } from 'src/utils/constants';
+import {
+  COLLECTION_IDENTIFIER_ERROR,
+  COLLECTION_IDENTIFIER_RGX,
+} from 'src/utils/constants';
 
 @InputType()
 export class CollectionStatsFilter {
   @Field(() => String)
-  @Matches(RegExp(NFT_IDENTIFIER_RGX), { message: NFT_IDENTIFIER_ERROR })
+  @Matches(RegExp(COLLECTION_IDENTIFIER_RGX), {
+    message: COLLECTION_IDENTIFIER_ERROR,
+  })
   identifier: string;
   constructor(init?: Partial<CollectionStatsFilter>) {
     Object.assign(this, init);
