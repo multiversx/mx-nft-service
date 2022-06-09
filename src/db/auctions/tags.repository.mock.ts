8c96bf2051bee5af6ef67029cbad34929d1eaadd
danielailie <danielailie12@gmail.com>
2022-06-09 17:01:56 +0300

    Fix tests

---
+++ src/db/auctions/tags.repository.mock.ts
@@ -0,0 +1,14 @@
+import { NftTag } from 'src/common/services/elrond-communication/models';
+import { EntityRepository, Repository } from 'typeorm';
+import { TagEntity } from './tags.entity';
+
+@EntityRepository(TagEntity)
+export class TagsRepositoryMock extends Repository<TagEntity> {
+  async getTags(_size: number): Promise<NftTag[]> {
+    return [];
+  }
+
+  async getTagsCount(): Promise<number> {
+    return 0;
+  }
+}
