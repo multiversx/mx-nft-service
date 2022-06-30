62464848d7ca46047cb3014134ec5f5109faa487
danielailie <danielailie12@gmail.com>
2022-06-30 10:56:33 +0300

    Update nftw type

--- src/db/migrations/1656575464518-AddNftFlags.ts
+++ src/db/migrations/1656575464518-AddNftFlags.ts
@@ -1,10 +1,10 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class AddFlags1656489314232 implements MigrationInterface {
-    name = 'AddFlags1656489314232'
+export class AddNftFlags1656575464518 implements MigrationInterface {
+    name = 'AddNftFlags1656575464518'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`nft_flags\` (\`id\` int NOT NULL AUTO_INCREMENT, \`identifier\` varchar(25) NOT NULL, \`nsfw\` decimal NOT NULL, INDEX \`IDX_37a67b537f8fe7c75e404c86e4\` (\`identifier\`), UNIQUE INDEX \`NftFlagsEntity_UQ_Flag\` (\`identifier\`, \`nsfw\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`nft_flags\` (\`id\` int NOT NULL AUTO_INCREMENT, \`identifier\` varchar(25) NOT NULL, \`nsfw\` decimal(6,2) NOT NULL, INDEX \`IDX_37a67b537f8fe7c75e404c86e4\` (\`identifier\`), UNIQUE INDEX \`NftFlagsEntity_UQ_Flag\` (\`identifier\`, \`nsfw\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
