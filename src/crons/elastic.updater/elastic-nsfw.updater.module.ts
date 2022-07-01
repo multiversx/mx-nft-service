3acdea0f8934e658dbd02ecbc912964741e035e4
johnykes <johnslymusic@gmail.com>
2022-07-01 20:18:50 +0300

    SERVICES-278 remove unnecessary import

--- src/crons/elastic.updater/elastic-nsfw.updater.module.ts
+++ src/crons/elastic.updater/elastic-nsfw.updater.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from 'src/common.module';
 import { AdminOperationsModuleGraph } from 'src/modules/admins/admin-operations.module';
-import { NftRarityModuleGraph } from 'src/modules/nft-rarity/nft-rarity.module';
 import { ElasticNsfwUpdaterService } from './elastic-nsfw.updater.service';
 
 @Module({
-  imports: [CommonModule, AdminOperationsModuleGraph, NftRarityModuleGraph],
+  imports: [CommonModule, AdminOperationsModuleGraph],
   providers: [ElasticNsfwUpdaterService],
 })
 export class ElasticNsfwUpdaterModule {}
