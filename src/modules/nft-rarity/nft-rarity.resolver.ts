f7fa89ea2c085465c34b7b053ddb70d95cbc8d36
johnykes <johnslymusic@gmail.com>
2022-07-04 14:57:41 +0300

    SERVICES-286 remove comments & unnecessary imports

--- src/modules/nft-rarity/nft-rarity.resolver.ts
+++ src/modules/nft-rarity/nft-rarity.resolver.ts
@@ -2,7 +2,6 @@ import { UseGuards } from '@nestjs/common';
 import { Args, Query, Resolver } from '@nestjs/graphql';
 import { ApolloError } from 'apollo-server-express';
 import { GqlAdminAuthGuard } from '../auth/gql-admin.auth-guard';
-import { GqlAuthGuard } from '../auth/gql.auth-guard';
 import { NftRarityService } from './nft-rarity.service';
 
 @Resolver()
