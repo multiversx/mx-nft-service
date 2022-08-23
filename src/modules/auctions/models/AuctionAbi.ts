3565f2a78dcbfa6a0e8b8c8eb0ded573ac503077
danielailie <danielailie12@gmail.com>
2022-08-23 16:13:44 +0300

    Add general abi interface

--- src/modules/auctions/models/AuctionAbi.ts
+++ src/modules/auctions/models/AuctionAbi.ts
@@ -7,10 +7,8 @@ import {
 } from '@elrondnetwork/erdjs';
 import { AuctionStatusEnum } from '.';
 
-export interface AuctionAbi {
-  auctioned_tokens: EsdtTokenPayment;
+export interface GeneralAuctionAbi {
   payment_token: TokenIdentifierValue;
-  payment_nonce: U64Value;
   auction_type: any;
   auction_status: AuctionStatusEnum;
   min_bid: BigUIntValue;
@@ -25,24 +23,18 @@ export interface AuctionAbi {
   creator_royalties_percentage: BigUIntValue;
 }
 
-export interface ExternalAuctionAbi {
+export interface AuctionAbi extends GeneralAuctionAbi {
+  auctioned_tokens: EsdtTokenPayment;
+  payment_nonce: U64Value;
+  min_bid_diff: BigUIntValue;
+}
+
+export interface ExternalAuctionAbi extends GeneralAuctionAbi {
+  payment_token_type: any;
+  payment_token_nonce: U64Value;
   auctioned_token_type: TokenIdentifierValue;
   auctioned_token_nonce: U64Value;
   nr_auctioned_tokens: BigUIntValue;
-  payment_token_type: TokenIdentifierValue;
-  payment_token_nonce: U64Value;
-  auction_type: any;
-  auction_status: AuctionStatusEnum;
-  min_bid: BigUIntValue;
-  min_bid_diff: BigUIntValue;
-  max_bid: OptionValue;
-  deadline: U64Value;
-  start_time: U64Value;
-  original_owner: AddressValue;
-  current_bid: BigUIntValue;
-  current_winner: AddressValue;
-  marketplace_cut_percentage: BigUIntValue;
-  creator_royalties_percentage: BigUIntValue;
 }
 
 export interface EsdtTokenPayment {
