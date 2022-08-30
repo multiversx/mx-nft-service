cf6f25d68fb4deb342a550ea2e0575cfd2fb3003
danielailie <danielailie12@gmail.com>
2022-08-30 14:55:11 +0300

    update last counter

--- src/modules/auctions/xoxno-reindex.service.ts
+++ src/modules/auctions/xoxno-reindex.service.ts
@@ -10,7 +10,7 @@ export class XoxnoReindexService {
 
   async handleReindexXoxno() {
     try {
-      const maxId = 287330;
+      const maxId = 291000;
       for (let index = 0; index < maxId; index++) {
         const auction = await this.auctionService.saveAuctionXoxno(
           index + 1,
