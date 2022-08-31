a02db39a67b980043331e8f5ff871e07f03c6f51
Traian Anghel <tanghel@live.com>
2022-08-31 21:49:00 +0300

    removed unused usings

--- src/crons/cache.warmer/cache.warmer.module.ts
+++ src/crons/cache.warmer/cache.warmer.module.ts
@@ -9,10 +9,6 @@ import { AuctionsWarmerService } from './auctions.warmer.service';
 import { CollectionsWarmerService } from './collections.warmer.service';
 import * as ormconfig from './../../ormconfig';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { MarketplacesService } from 'src/modules/marketplaces/marketplaces.service';
-import { MarketplaceRepository } from 'src/db/marketplaces/marketplaces.repository';
-import { MarketplaceCollectionsRepository } from 'src/db/marketplaces/marketplace-collections.repository';
-import { MarketplacesCachingService } from 'src/modules/marketplaces/marketplaces-caching.service';
 import { MarketplacesModuleGraph } from 'src/modules/marketplaces/marketplaces.module';
 import { TokensWarmerService } from './tokens.warmer.service';
 
