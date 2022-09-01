4609c71f3aee3e9ac001d0677d00577cc656a8bb
danielailie <danielailie12@gmail.com>
2022-09-01 19:54:58 +0300

    Fix marketplace mapping

--- src/modules/auctions/auctions-queries.resolver.ts
+++ src/modules/auctions/auctions-queries.resolver.ts
@@ -238,20 +238,26 @@ export class AuctionsQueriesResolver extends BaseResolver(Auction) {
 
   @ResolveField('marketplace', () => Marketplace)
   async marketplace(@Parent() auction: Auction) {
-    const { marketplaceKey, identifier, id, marketplaceAuctionId, asset } =
-      auction;
+    const { marketplaceKey, identifier, id, marketplaceAuctionId } = auction;
 
     if (!marketplaceKey) return null;
     const marketplace = await this.marketplaceProvider.load(marketplaceKey);
     const marketplaceValue = marketplace?.value;
+    if (
+      marketplaceValue?.length > 0 &&
+      marketplaceValue[0].marketplaceKey === 'xoxno'
+    ) {
+      const asset = await this.assetsProvider.load(identifier);
+      const assetValue = asset?.value;
+      return Marketplace.fromEntityForXoxno(
+        marketplaceValue[0],
+        identifier,
+        marketplaceAuctionId,
+        assetValue.type,
+      );
+    }
     return marketplaceValue?.length > 0
-      ? Marketplace.fromEntity(
-          marketplaceValue[0],
-          identifier,
-          id,
-          marketplaceAuctionId,
-          asset?.type,
-        )
+      ? Marketplace.fromEntity(marketplaceValue[0], identifier, id)
       : null;
   }
 
--- src/modules/marketplaces/models/Marketplace.dto.ts
+++ src/modules/marketplaces/models/Marketplace.dto.ts
@@ -33,21 +33,33 @@ export class Marketplace {
     entity: MarketplaceEntity,
     identifier?: string,
     id?: number,
-    marketplaceAuctionId?: number,
-    nftType?: NftTypeEnum,
   ) {
     let url = identifier ? `${entity.url}${identifier}` : entity.url;
     if (entity.type === MarketplaceTypeEnum.Internal) {
       url = identifier && id ? `${url}/auction/${id}` : url;
     }
 
-    console.log({ entity, marketplaceAuctionId, nftType });
-    if (
-      entity.key === 'xoxno' &&
-      marketplaceAuctionId &&
-      nftType === NftTypeEnum.SemiFungibleESDT
-    ) {
-      url = identifier && id ? `${url}-${id}` : url;
+    return entity && Object.keys(entity).length > 0
+      ? new Marketplace({
+          address: entity.address,
+          name: entity.name,
+          url: url,
+          key: entity.key,
+          type: entity.type,
+        })
+      : null;
+  }
+
+  static fromEntityForXoxno(
+    entity: MarketplaceEntity,
+    identifier: string,
+    marketplaceAuctionId: number,
+    nftType: NftTypeEnum,
+  ) {
+    let url = identifier ? `${entity.url}${identifier}` : entity.url;
+
+    if (marketplaceAuctionId && nftType === NftTypeEnum.SemiFungibleESDT) {
+      url = marketplaceAuctionId ? `${url}-${marketplaceAuctionId}` : url;
     }
     return entity && Object.keys(entity).length > 0
       ? new Marketplace({
