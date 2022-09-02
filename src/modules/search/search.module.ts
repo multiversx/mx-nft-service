5751c6a6c93325001da9cbe5cfcdf17cd37809fb
Traian Anghel <tanghel@live.com>
2022-09-02 19:16:36 +0300

    search verified collections first

--- src/modules/search/search.module.ts
+++ src/modules/search/search.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ElrondCommunicationModule } from 'src/common';
+import { CollectionModuleGraph } from '../nftCollections/collection.module';
 import { SearchResolver } from './search.resolver';
 import { SearchService } from './search.service';
 
 @Module({
   providers: [SearchService, SearchResolver],
-  imports: [ElrondCommunicationModule],
+  imports: [ElrondCommunicationModule, CollectionModuleGraph],
   exports: [SearchService],
 })
 export class SearchModuleGraph {}
