3e271e8cd3538612c014121e45c5339f281c127e
danielailie <danielailie12@gmail.com>
2022-09-19 13:22:15 +0300

    Add artist on asset resource

--- src/modules/assets/assets-queries.resolver.ts
+++ src/modules/assets/assets-queries.resolver.ts
@@ -32,12 +32,14 @@ import { InternalMarketplaceProvider } from './loaders/internal-marketplace.load
 import { Marketplace } from '../marketplaces/models';
 import { MarketplaceFilters } from '../marketplaces/models/Marketplace.Filter';
 import { LowestAuctionForMarketplaceProvider } from '../auctions/loaders/lowest-auctions-for-marketplace.loader';
+import { SmartContractOwnerProvider } from './loaders/artists.loader';
 
 @Resolver(() => Asset)
 export class AssetsQueriesResolver extends BaseResolver(Asset) {
   constructor(
     private assetsService: AssetsGetterService,
     private accountsProvider: AccountsProvider,
+    private smartContractOwnerProvider: SmartContractOwnerProvider,
     private assetsLikeProvider: AssetLikesProvider,
     private assetsViewsProvider: AssetsViewsLoader,
     private isAssetLikedProvider: IsAssetLikedProvider,
@@ -177,6 +179,28 @@ export class AssetsQueriesResolver extends BaseResolver(Asset) {
     if (!creatorAddress) return null;
     const account = await this.accountsProvider.load(creatorAddress);
     return Account.fromEntity(account?.value, creatorAddress);
+  }
+
+  @ResolveField('artist', () => Account)
+  async artist(@Parent() asset: Asset) {
+    const { creatorAddress } = asset;
+
+    const address = new Address(creatorAddress);
+    let artistAddress: string = creatorAddress;
+
+    if (!creatorAddress) return null;
+
+    if (address.isContractAddress()) {
+      const response = await this.smartContractOwnerProvider.load(
+        creatorAddress,
+      );
+      artistAddress = response?.value
+        ? response?.value?.ownerAddress
+        : creatorAddress;
+    }
+
+    const account = await this.accountsProvider.load(artistAddress);
+    return Account.fromEntity(account?.value, artistAddress);
   }
 
   @ResolveField(() => Account)
---
+++ src/modules/assets/loaders/artists.redis-handler.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { RedisCacheService } from 'src/common';
+import { RedisKeyValueDataloaderHandler } from 'src/modules/common/redis-key-value-dataloader.handler';
+import { RedisValue } from 'src/modules/common/redis-value.dto';
+import { TimeConstants } from 'src/utils/time-utils';
+
+@Injectable()
+export class SmartContractOwnerRedisHandler extends RedisKeyValueDataloaderHandler<string> {
+  constructor(redisCacheService: RedisCacheService) {
+    super(redisCacheService, 'smartContractOwner');
+  }
+
+  mapValues(
+    returnValues: { key: string; value: any }[],
+    accountsAddreses: { [key: string]: any[] },
+  ) {
+    const redisValues = [];
+    for (const item of returnValues) {
+      if (item.value === null) {
+        item.value = accountsAddreses[item.key]
+          ? accountsAddreses[item.key][0]
+          : null;
+        redisValues.push(item);
+      }
+    }
+
+    return [
+      new RedisValue({
+        values: redisValues,
+        ttl: TimeConstants.oneWeek,
+      }),
+    ];
+  }
+}
