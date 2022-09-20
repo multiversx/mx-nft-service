43191b00103e691f8171567f46e83b97d41e8aed
danielailie <danielailie12@gmail.com>
2022-09-20 15:29:51 +0300

    Add description for assets on collection

--- src/modules/nftCollections/models/Collection.dto.ts
+++ src/modules/nftCollections/models/Collection.dto.ts
@@ -1,7 +1,7 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
 import { CollectionApi, RolesApi } from 'src/common';
 import { Account } from 'src/modules/account-stats/models';
-import { Asset, AssetsResponse } from 'src/modules/assets/models';
+import { Asset } from 'src/modules/assets/models';
 import { NftTypeEnum } from 'src/modules/assets/models/NftTypes.enum';
 import { CollectionAsset } from './CollectionAsset.dto';
 import { CollectionSocial } from './CollectionSocial.dto';
@@ -22,7 +22,10 @@ export class Collection {
   artist: Account;
   @Field(() => CollectionAsset, { nullable: true })
   collectionAsset: CollectionAsset;
-  @Field(() => [Asset], { nullable: 'itemsAndList' })
+  @Field(() => [Asset], {
+    nullable: 'itemsAndList',
+    description: 'This will return only the first 10 assets',
+  })
   assets: Asset[];
   @Field()
   name: string;
