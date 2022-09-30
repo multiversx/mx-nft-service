81d45f29018040cd82015c4f55e27ee7edf7182a
johnykes <johnslymusic@gmail.com>
2022-09-30 12:06:32 +0300

    SERVICES-679 make params optional instead of setting them as undefined

--- src/common/persistence/persistence.service.ts
+++ src/common/persistence/persistence.service.ts
@@ -914,8 +914,8 @@ export class PersistenceService {
   }
 
   async getMostLikedAssetsIdentifiers(
-    offset: number = undefined,
-    limit: number = undefined,
+    offset?: number,
+    limit?: number,
   ): Promise<AssetLikeEntity[]> {
     return await this.execute(
       this.getMostLikedAssetsIdentifiers.name,
