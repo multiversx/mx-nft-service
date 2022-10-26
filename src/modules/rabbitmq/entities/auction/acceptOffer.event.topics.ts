0ea8c96e74b7941bc5fc975ec4d93534bc100969
danielailie <danielailie12@gmail.com>
2022-10-26 16:11:53 +0300

    Add mutations for offers

--- src/modules/rabbitmq/entities/auction/acceptOffer.event.topics.ts
+++ src/modules/rabbitmq/entities/auction/acceptOffer.event.topics.ts
@@ -48,6 +48,30 @@ export class AcceptOfferEventsTopics {
     this.auctionId = parseInt(
       BinaryUtils.tryBase64ToBigInt(rawTopics[14])?.toString() ?? '0',
     );
+    this.collection = Buffer.from(rawTopics[2], 'base64').toString();
+    this.nonce = Buffer.from(rawTopics[3], 'base64').toString('hex');
+    this.nrOfferTokens = parseInt(
+      BinaryUtils.tryBase64ToBigInt(rawTopics[4])?.toString() ?? '1',
+    );
+    this.paymentTokenIdentifier = Buffer.from(
+      rawTopics[5],
+      'base64',
+    ).toString();
+    this.paymentTokenNonce = parseInt(
+      BinaryUtils.tryBase64ToBigInt(rawTopics[6])?.toString() ?? '0',
+    );
+    this.paymentAmount = Buffer.from(rawTopics[7], 'base64')
+      .toString('hex')
+      .hexBigNumberToString();
+    this.offerOwner = new Address(Buffer.from(rawTopics[8], 'base64'));
+    this.startdate = parseInt(
+      Buffer.from(rawTopics[9], 'base64').toString('hex'),
+      16,
+    ).toString();
+    this.enddate = parseInt(
+      Buffer.from(rawTopics[10], 'base64').toString('hex'),
+      16,
+    ).toString();
   }
 
   toPlainObject() {
