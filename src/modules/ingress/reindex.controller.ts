1b8f6c691b5a85572c205dfeda8994cd05e6e9d4
johnykes <johnslymusic@gmail.com>
2022-11-07 16:41:30 +0200

    replace GET with POST, becase no data is fetched

--- src/modules/ingress/reindex.controller.ts
+++ src/modules/ingress/reindex.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { NsfwUpdaterService } from 'src/crons/elastic.updater/nsfw.updater.service';
 import { RarityUpdaterService } from 'src/crons/elastic.updater/rarity.updater.service';
 import { NftScamService } from '../nft-scam/nft-scam.service';
@@ -13,27 +13,27 @@ export class ReindexController {
     private nftScamService: NftScamService,
   ) {}
 
-  @Get('/trigger-nsfw-reindex')
+  @Post('/trigger-nsfw-reindex')
   async triggerNsfwReindex(): Promise<void> {
     await this.nsfwRService.updateNsfwWhereNone();
   }
 
-  @Get('/trigger-rarity-reindex')
+  @Post('/trigger-rarity-reindex')
   async triggerRarityReindex(): Promise<void> {
     this.rarityUpdaterService.handleReindexAllTokenRarities();
   }
 
-  @Get('/trigger-rarity-validation')
+  @Post('/trigger-rarity-validation')
   async triggerRarityValidation(): Promise<void> {
     this.rarityUpdaterService.handleValidateAllTokenRarities();
   }
 
-  @Get('/trigger-traits-reindex')
+  @Post('/trigger-traits-reindex')
   async triggerTraitsReindex(): Promise<void> {
     this.nftTraitsService.updateAllCollectionTraits();
   }
 
-  @Get('/trigger-scam-reindex')
+  @Post('/trigger-scam-reindex')
   async triggerScamReindex(): Promise<void> {
     this.nftScamService.validateOrUpdateAllNftsScamInfo();
   }
