c6d74cbe42be7cc8441688b0f60159b22dc7aba9
danielailie <danielailie12@gmail.com>
2022-11-10 18:22:19 +0200

    Add offers stats for account end nfts

--- src/modules/explore-stats/explore-stats.module.ts
+++ src/modules/explore-stats/explore-stats.module.ts
@@ -4,6 +4,7 @@ import { ExploreStatsResolver } from './explore-stats.resolver';
 import { ExploreStatsService } from './explore-stats.service';
 import { CollectionsModuleGraph } from '../nftCollections/collections.module';
 import { AuctionsModuleGraph } from '../auctions/auctions.module';
+import { OffersModuleGraph } from '../offers/offers.module';
 
 @Module({
   providers: [ExploreStatsService, ExploreStatsResolver],
@@ -11,6 +12,7 @@ import { AuctionsModuleGraph } from '../auctions/auctions.module';
     ElrondCommunicationModule,
     CollectionsModuleGraph,
     AuctionsModuleGraph,
+    OffersModuleGraph,
   ],
   exports: [ExploreStatsService],
 })
