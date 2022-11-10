dfc657dfc042512409299190d317f9c9f825bf90
danielailie <danielailie12@gmail.com>
2022-11-10 18:22:19 +0200

    Add offers stats for account end nfts

--- src/modules/account-stats/accounts-stats.module.ts
+++ src/modules/account-stats/accounts-stats.module.ts
@@ -6,6 +6,7 @@ import { AccountsStatsCachingService } from './accounts-stats.caching.service';
 import { MarketplacesService } from '../marketplaces/marketplaces.service';
 import { MarketplacesCachingService } from '../marketplaces/marketplaces-caching.service';
 import { CollectionsModuleGraph } from '../nftCollections/collections.module';
+import { OffersModuleGraph } from '../offers/offers.module';
 
 @Module({
   providers: [
@@ -19,6 +20,7 @@ import { CollectionsModuleGraph } from '../nftCollections/collections.module';
   imports: [
     MxCommunicationModule,
     forwardRef(() => CollectionsModuleGraph),
+    forwardRef(() => OffersModuleGraph),
   ],
   exports: [AccountsStatsService],
 })
