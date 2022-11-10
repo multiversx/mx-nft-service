e9b108a5dfb430c83b6d3ccca6af52b7c96c2cbe
johnykes <johnslymusic@gmail.com>
2022-11-10 12:08:44 +0200

    simplify nft-scam updater service (locker & try-catch used anyway in the validateOrUpdateAllNftsScamInfo function)

--- src/crons/elastic.updater/nft-scam.updater.service.ts
+++ src/crons/elastic.updater/nft-scam.updater.service.ts
@@ -1,31 +1,11 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { Locker } from 'src/utils/locker';
+import { Injectable } from '@nestjs/common';
 import { NftScamService } from 'src/modules/nft-scam/nft-scam.service';
 
 @Injectable()
 export class NftScamUpdaterService {
-  constructor(
-    private readonly nftScamService: NftScamService,
-    private readonly logger: Logger,
-  ) {}
+  constructor(private readonly nftScamService: NftScamService) {}
 
   async handleUpdateScamInfoWhereNotSetOrOutdated(): Promise<void> {
-    try {
-      await Locker.lock(
-        `handleUpdateScamInfoWhereNotSetOrOutdated`,
-        async () => {
-          await this.nftScamService.validateOrUpdateAllNftsScamInfo();
-        },
-        true,
-      );
-    } catch (error) {
-      this.logger.error(
-        `Error when setting/updating scam info where not set / outdated`,
-        {
-          path: `${NftScamUpdaterService.name}.${this.handleUpdateScamInfoWhereNotSetOrOutdated.name}`,
-          exception: error?.message,
-        },
-      );
-    }
+    await this.nftScamService.validateOrUpdateAllNftsScamInfo();
   }
 }
