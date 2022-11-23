a9854e92e841fe3f20a19e8077a9c95d853275dd
johnykes <48965850+johnykes@users.noreply.github.com>
2022-11-23 17:45:48 +0200

    Services 606 update npm version for nft service (#698)
    * migrate apollo-server-express -> apollo-server
    
    * specifically add graphqlUploadExpress to app so it works the same as before
    
    * upgrade to apollo-server 4 and remove unnecessary import
    
    * remove graphql-upload workaround
    
    * remove unnecessary apollo 4 (for now) and the auto update sign ^
    
    * SERVICES-606 upgrade npm and few packages; remove nestjs-redis (needs to be replaced)
    
    * SERVICES-606 replace nestjs-redis with custom code
    
    * SERVICES-606 add ioredis
    
    * SERVICES-606 readd class-transformer
    
    * SERVICES-606 update graphql and graphql-upload
    
    * SERVICES-606 remove npm auto update for all packets
    
    * SERVICES-606 remove unused mysql
    
    * SERVICES-606 remove & auto-update for nestjs/testing

--- src/common.module.ts
+++ src/common.module.ts
@@ -1,9 +1,7 @@
-import { forwardRef, Logger, Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config/dist';
 import { ScheduleModule } from '@nestjs/schedule';
-import { RedisModule } from 'nestjs-redis';
 import { ElrondCommunicationModule } from './common/services/elrond-communication/elrond-communication.module';
-import { cacheConfig } from './config';
 import { CachingModule } from './common/services/caching/caching.module';
 import { ApiConfigService } from './utils/api.config.service';
 
@@ -12,64 +10,6 @@ import { ApiConfigService } from './utils/api.config.service';
     forwardRef(() => CachingModule),
     ScheduleModule.forRoot(),
     ConfigModule,
-
-    RedisModule.register([
-      {
-        host: process.env.REDIS_URL,
-        port: parseInt(process.env.REDIS_PORT),
-        password: process.env.REDIS_PASSWORD,
-        db: 0,
-      },
-      {
-        clientName: cacheConfig.auctionsRedisClientName,
-        host: process.env.REDIS_URL,
-        port: parseInt(process.env.REDIS_PORT),
-        password: process.env.REDIS_PASSWORD,
-        db: cacheConfig.auctionsDbName,
-      },
-      {
-        clientName: cacheConfig.ordersRedisClientName,
-        host: process.env.REDIS_URL,
-        port: parseInt(process.env.REDIS_PORT),
-        password: process.env.REDIS_PASSWORD,
-        db: cacheConfig.ordersDbName,
-      },
-      {
-        clientName: cacheConfig.assetsRedisClientName,
-        host: process.env.REDIS_URL,
-        port: parseInt(process.env.REDIS_PORT),
-        password: process.env.REDIS_PASSWORD,
-        db: cacheConfig.assetsDbName,
-      },
-      {
-        clientName: cacheConfig.persistentRedisClientName,
-        host: process.env.REDIS_URL,
-        port: parseInt(process.env.REDIS_PORT),
-        password: process.env.REDIS_PASSWORD,
-        db: cacheConfig.persistentDbName,
-      },
-      {
-        clientName: cacheConfig.collectionsRedisClientName,
-        host: process.env.REDIS_URL,
-        port: parseInt(process.env.REDIS_PORT),
-        password: process.env.REDIS_PASSWORD,
-        db: cacheConfig.collectionsDbName,
-      },
-      {
-        clientName: cacheConfig.rarityQueueClientName,
-        host: process.env.REDIS_URL,
-        port: parseInt(process.env.REDIS_PORT),
-        password: process.env.REDIS_PASSWORD,
-        db: cacheConfig.rarityQueueDbName,
-      },
-      {
-        clientName: cacheConfig.traitsQueueClientName,
-        host: process.env.REDIS_URL,
-        port: parseInt(process.env.REDIS_PORT),
-        password: process.env.REDIS_PASSWORD,
-        db: cacheConfig.traitsQueueDbName,
-      },
-    ]),
     ElrondCommunicationModule,
   ],
   exports: [ElrondCommunicationModule, CachingModule, ApiConfigService],
