2ead72c75003134f33b89a1a16ab7fea09290eb9
johnykes <48965850+johnykes@users.noreply.github.com>
2022-12-05 18:52:16 +0200

    SERVICES-995 separate asset history caching service; cache invalidation (#727)
    * SERVICES-995 separate asset history caching service; cache invalidation
    
    * SERVICES-995 typo

--- src/modules/rabbitmq/cache-invalidation/cache-invalidation-module/cache-invalidation-events.module.ts
+++ src/modules/rabbitmq/cache-invalidation/cache-invalidation-module/cache-invalidation-events.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { AssetsHistoryCachingService } from 'src/modules/asset-history/assets-history-caching.service';
 import { AssetsLikesCachingService } from 'src/modules/assets/assets-likes.caching.service';
 import { IsAssetLikedRedisHandler } from 'src/modules/assets/loaders/asset-is-liked.redis-handler';
 import { AuctionsCachingModule } from 'src/modules/auctions/caching/auctions-caching.module';
@@ -13,6 +14,7 @@ import { CacheInvalidationEventsService } from './cache-invalidation-events.serv
     NotificationsCachingService,
     AssetsLikesCachingService,
     IsAssetLikedRedisHandler,
+    AssetsHistoryCachingService,
   ],
   exports: [CacheInvalidationEventsService, NotificationsCachingService],
 })
