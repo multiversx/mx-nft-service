30c6c77714a90e1945ee7ab7c6d4712355e5871f
johnykes <johnslymusic@gmail.com>
2022-12-06 13:56:59 +0200

    SERVICES-989 remove duplicate parameters

--- src/modules/marketplaces/marketplaces-events-indexing.service.ts
+++ src/modules/marketplaces/marketplaces-events-indexing.service.ts
@@ -53,11 +53,9 @@ export class MarketplaceEventsIndexingService {
       [newestTimestamp, oldestTimestamp] = await this.getEventsAndSaveToDb(
         marketplaceAddress,
         size,
-        newestTimestamp,
-        oldestTimestamp,
-        stopIfDuplicates,
         beforeTimestamp,
         afterTimestamp,
+        stopIfDuplicates,
       );
 
       if (
@@ -86,16 +84,14 @@ export class MarketplaceEventsIndexingService {
     newestTimestamp: number,
     oldestTimestamp: number,
     stopIfDuplicates?: boolean,
-    beforeTimestamp?: number,
-    afterTimestamp?: number,
   ): Promise<[number, number]> {
     do {
       const [batch, batchSize, timestamp] =
         await this.elrondElasticService.getAddressHistory(
           marketplaceAddress,
           size,
-          oldestTimestamp ?? beforeTimestamp,
-          afterTimestamp,
+          oldestTimestamp,
+          newestTimestamp,
         );
 
       if (batchSize === 0) {
