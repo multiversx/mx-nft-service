c3d2cb6602f1fc503d01b9523ca724836ba1573b
danielailie <danielailie12@gmail.com>
2022-12-14 13:55:30 +0200

    Update condition

--- src/modules/auth/auth.utils.ts
+++ src/modules/auth/auth.utils.ts
@@ -1,9 +1,9 @@
 export class AuthUtils {
   static bypassAuthorizationOnTestnet(request: any) {
     if (
+      !!request.headers['x-nft-address'] &&
       (process.env.NODE_ENV === 'development' ||
-        process.env.NODE_ENV === 'test') &&
-      !!request.headers['x-nft-address']
+        process.env.NODE_ENV === 'test')
     ) {
       const address = request.headers['x-nft-address'];
       request.auth = {
