c6d914023e54ba2dd9ca5daad58e2d82c9fe12ee
johnykes <johnslymusic@gmail.com>
2022-12-19 11:39:09 +0200

    SERVICES-1020 timeout 3000 -> 500 ms

--- src/common/services/elrond-communication/elrond-tools.service.ts
+++ src/common/services/elrond-communication/elrond-tools.service.ts
@@ -87,7 +87,7 @@ export class ElrondToolsService {
     const accessTokenInfo = await this.nativeAuthSigner.getToken();
     return {
       authorization: `Bearer ${accessTokenInfo.token}`,
-      timeout: 3000,
+      timeout: 500,
     };
   }
 
