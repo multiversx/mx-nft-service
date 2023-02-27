baafb32dfc6d9df8cf7576d85399a424e3dc5eae
danielailie <danielailie12@gmail.com>
2023-02-27 16:41:54 +0200

    Add logginh

--- src/modules/rabbitmq/cache-invalidation/cache-invalidation-publisher/change-events-publisher.service.ts
+++ src/modules/rabbitmq/cache-invalidation/cache-invalidation-publisher/change-events-publisher.service.ts
@@ -25,6 +25,7 @@ export class NftLikePublisherService {
   ) {}
 
   async publish(payload: UserNftLikeEvent) {
+    console.log('publish like event ', { payload });
     await this.rabbitPublisherService.publish(
       rabbitExchanges.NFT_LIKE,
       payload,
