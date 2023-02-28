ababdf9993737ecc45d8c08ca35b47accd08ee88
johnykes <johnslymusic@gmail.com>
2023-02-28 15:44:52 +0200

    set default paymentNonce if not already set

--- src/db/auctions/auction.entity.ts
+++ src/db/auctions/auction.entity.ts
@@ -97,6 +97,9 @@ export class AuctionEntity extends BaseEntity {
   constructor(init?: Partial<AuctionEntity>) {
     super();
     Object.assign(this, init);
+    if (!this.paymentNonce) {
+      this.paymentNonce = 0;
+    }
   }
 
   static fromAuctionAbi(
