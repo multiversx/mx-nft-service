a82a035e413b7c94f2d35ae3d621b031c0b419dc
danielailie <danielailie12@gmail.com>
2023-02-28 11:09:21 +0200

    More logging

--- src/modules/nftCollections/collections-queries.resolver.ts
+++ src/modules/nftCollections/collections-queries.resolver.ts
@@ -57,7 +57,7 @@ export class CollectionsQueriesResolver extends BaseResolver(Collection) {
     pagination: ConnectionArgs,
   ): Promise<CollectionResponse> {
     const { limit, offset } = pagination.pagingParams();
-    console.log({ filters });
+    console.log({ filters, sorting });
     const [collections, count] =
       await this.collectionsGetterService.getCollections(
         offset,
