54579c14ee9ace4bad28cc3d95efab70a74ffcce
danielailie <danielailie12@gmail.com>
2023-02-28 11:30:32 +0200

    Revert logging condition

--- src/modules/assets/loaders/assets-collection.loader.ts
+++ src/modules/assets/loaders/assets-collection.loader.ts
@@ -35,7 +35,7 @@ export class AssetsCollectionsProvider extends BaseProvider<string> {
     const nftsPromisesResponse = await Promise.all(nftsPromises);
     let response: any = {};
     for (const [nfts, count] of nftsPromisesResponse) {
-      if (nfts?.length) console.log('here ', { nfts, identifiers });
+      if (!nfts?.length) console.log('here ', { nfts, identifiers });
       const key = `${nfts[0]?.collection}_${page}_${size}`;
       response[key] = { nfts: nfts, count: count };
     }
