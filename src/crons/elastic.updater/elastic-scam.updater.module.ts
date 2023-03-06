b197904125e773a22daa9602a0ff560f29127eac
danielailie <danielailie12@gmail.com>
2023-03-06 15:37:26 +0200

    Remove typeorm dependencies from scam module

--- src/crons/elastic.updater/elastic-scam.updater.module.ts
+++ src/crons/elastic.updater/elastic-scam.updater.module.ts
@@ -1,19 +1,12 @@
 import { Logger, Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommonModule } from 'src/common.module';
 import { DocumentDbModule } from 'src/document-db/document-db.module';
 import { ScamModule } from 'src/modules/scam/scam.module';
-import ormconfig from 'src/ormconfig';
 import { ElasticScamUpdaterService } from './elastic-scam.service';
 import { NftScamUpdaterService } from './nft-scam.updater.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({ ...ormconfig, keepConnectionAlive: true }),
-    CommonModule,
-    ScamModule,
-    DocumentDbModule,
-  ],
+  imports: [CommonModule, ScamModule, DocumentDbModule],
   providers: [Logger, ElasticScamUpdaterService, NftScamUpdaterService],
   exports: [ElasticScamUpdaterService, NftScamUpdaterService],
 })
--- src/crons/elastic.updater/elastic-traits.updater.module.ts
+++ src/crons/elastic.updater/elastic-traits.updater.module.ts
@@ -1,8 +1,6 @@
 import { Logger, Module } from '@nestjs/common';
 import { CommonModule } from 'src/common.module';
 import { CacheModule } from 'src/common/services/caching/caching.module';
-import * as ormconfig from './../../ormconfig';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { NftTraitsModule } from 'src/modules/nft-traits/nft-traits.module';
 import { TraitsUpdaterService } from './traits.updater.service';
 import { ElasticTraitsUpdaterService } from './elastic-traits.updater.service';
