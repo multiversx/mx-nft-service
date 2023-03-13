4f8b2a8ae839f0914cb622cda02ccfbd11c28dc0
danielailie <danielailie12@gmail.com>
2023-03-13 14:36:45 +0200

    Add tymescaledb config

---
+++ src/timescaledb/analytics-data.module.ts
@@ -0,0 +1,52 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommonModule } from 'src/common.module';
+import { CacheModule } from 'src/common/services/caching/caching.module';
+import { ApiConfigService } from 'src/modules/common/api-config/api.config.service';
+import { AnalyticsDataGetterService } from './analytics-data.getter.service';
+import { AnalyticsDataSetterService } from './analytics-data.setter.service';
+import {
+  CloseDaily,
+  CloseHourly,
+  SumDaily,
+  SumHourly,
+  TokenBurnedWeekly,
+  XNftsAnalyticsEntity,
+} from './entities/analytics.entities';
+
+@Module({
+  imports: [
+    CommonModule,
+    CacheModule,
+    TypeOrmModule.forRootAsync({
+      imports: [CommonModule],
+      useFactory: (apiConfig: ApiConfigService) => ({
+        type: 'postgres',
+        host: apiConfig.getTimescaleDbHost(),
+        port: apiConfig.getTimescaleDbPort(),
+        database: apiConfig.getTimescaleDbDatabase(),
+        username: apiConfig.getTimescaleDbUsername(),
+        password: apiConfig.getTimescaleDbPassword(),
+        ssl: true,
+        extra: {
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+        entities: ['dist/**/*.entities.{ts,js}'],
+      }),
+      inject: [ApiConfigService],
+    }),
+    TypeOrmModule.forFeature([
+      XNftsAnalyticsEntity,
+      SumDaily,
+      SumHourly,
+      CloseDaily,
+      CloseHourly,
+      TokenBurnedWeekly,
+    ]),
+  ],
+  providers: [AnalyticsDataGetterService, AnalyticsDataSetterService],
+  exports: [AnalyticsDataGetterService, AnalyticsDataSetterService],
+})
+export class AnalyticsDataModule {}
