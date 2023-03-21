9b33720c3550c17d7f614a119a6f0f410dd7606c
johnykes <johnslymusic@gmail.com>
2023-03-21 12:14:03 +0200

    SERVICES-1085 marketplace reindex state DB validation inside handlers

--- src/modules/marketplaces/marketplaces-reindex-handlers/reindex-auction-started.handler.ts
+++ src/modules/marketplaces/marketplaces-reindex-handlers/reindex-auction-started.handler.ts
@@ -1,5 +1,6 @@
 import { BinaryUtils } from '@multiversx/sdk-nestjs-common';
 import { Injectable } from '@nestjs/common';
+import { constants } from 'src/config';
 import { AuctionEntity } from 'src/db/auctions';
 import { AuctionStatusEnum } from 'src/modules/auctions/models';
 import { Token } from 'src/modules/usdPrice/Token.model';
@@ -17,6 +18,10 @@ export class ReindexAuctionStartedHandler {
     const itemsCount = parseInt(input.itemsCount);
     const modifiedDate = DateUtils.getUtcDateFromTimestamp(input.timestamp);
     const startTime = Number.isNaN(input.startTime) ? input.timestamp : input.startTime;
+    const endTime = input.endTime > 0 ? input.endTime : 0;
+    const minBidDenominated = BigNumberUtils.denominateAmount(input.minBid, paymentToken.decimals);
+    const maxBidDenominated = BigNumberUtils.denominateAmount(input.maxBid !== 'NaN' ? input.maxBid : '0', paymentToken.decimals);
+
     const auction = new AuctionEntity({
       creationDate: modifiedDate,
       modifiedDate,
@@ -33,11 +38,11 @@ export class ReindexAuctionStartedHandler {
       ownerAddress: input.sender,
       minBid: input.minBid,
       maxBid: input.maxBid !== 'NaN' ? input.maxBid : '0',
-      minBidDenominated: BigNumberUtils.denominateAmount(input.minBid, paymentToken.decimals),
-      maxBidDenominated: BigNumberUtils.denominateAmount(input.maxBid !== 'NaN' ? input.maxBid : '0', paymentToken.decimals),
+      minBidDenominated: Math.min(minBidDenominated, constants.dbMaxDenominatedValue),
+      maxBidDenominated: Math.min(maxBidDenominated, constants.dbMaxDenominatedValue),
       minBidDiff: input.minBidDiff ?? '0',
-      startDate: startTime,
-      endDate: input.endTime > 0 ? input.endTime : 0,
+      startDate: Math.min(startTime, constants.dbMaxTimestamp),
+      endDate: Math.min(endTime, constants.dbMaxTimestamp),
       tags: '',
       blockHash: input.blockHash ?? '',
       marketplaceKey: marketplaceReindexState.marketplace.key,
