d465141201d676674b1c000bd4a38c239fd68a5a
danielailie <danielailie12@gmail.com>
2023-03-22 13:50:58 +0200

    Add none scam info

--- src/crons/elastic.updater/elastic-scam.service.ts
+++ src/crons/elastic.updater/elastic-scam.service.ts
@@ -6,7 +6,7 @@ import { NftScamUpdaterService } from './nft-scam.updater.service';
 export class ElasticScamUpdaterService {
   constructor(private readonly nftScamUpdaterService: NftScamUpdaterService) {}
 
-  @Cron(CronExpression.EVERY_DAY_AT_8AM)
+  @Cron(CronExpression.EVERY_10_MINUTES)
   async handleUpdateScamInfoWhereNotSetOrOutdatedCronJob() {
     await this.nftScamUpdaterService.handleUpdateScamInfoWhereNotSetOrOutdated();
   }
