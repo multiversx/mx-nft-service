3f6c54217ba35e755dda83f93b4e6f8ac1a9ef0a
danielailie <danielailie12@gmail.com>
2023-05-02 12:48:09 +0300

    add analytics indexer

---
+++ src/analytics.indexer.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import BigNumber from 'bignumber.js';
+import { AnalyticsModule } from './crons/analytics/analytics.module';
+import { AnalyticsService } from './crons/analytics/analytics.service';
+
+export async function run(startDateUtc: string, endDateUtc: string) {
+  BigNumber.config({ EXPONENTIAL_AT: [-100, 100] });
+  const app = await NestFactory.create(AnalyticsModule);
+  const analyticsService = app.get<AnalyticsService>(AnalyticsService);
+  await analyticsService.reindexTrendingCollections(
+    parseInt(startDateUtc),
+    parseInt(endDateUtc),
+  );
+  return process.exit(0);
+}
+
+run(process.argv[2], process.argv[3]);
