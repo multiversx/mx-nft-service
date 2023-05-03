c2c24db8bd45bf72ebbcfcd73191b2b61927a522
danielailie <danielailie12@gmail.com>
2023-05-03 10:20:50 +0300

    Clean up logs

--- src/utils/analytics.utils.ts
+++ src/utils/analytics.utils.ts
@@ -4,7 +4,7 @@ import BigNumber from 'bignumber.js';
 import * as moment from 'moment';
 
 export const decodeTime = (time: string): [string, moment.unitOfTime.Base] => {
-  const [timeAmount, timeUnit] = time.match(/[a-zA-Z]+|[0-9]+/g);
+  const [timeAmount, timeUnit] = time?.match(/[a-zA-Z]+|[0-9]+/g);
   return [timeAmount, timeUnit as moment.unitOfTime.Base];
 };
 
@@ -15,11 +15,12 @@ export const computeTimeInterval = (
   const [timeAmount, timeUnit] = decodeTime(time);
 
   const startDate1 = moment().utc().subtract(timeAmount, timeUnit);
+
   const startDate2 = start ? moment.unix(parseInt(start)).utc() : undefined;
 
   const startDate = startDate2
     ? moment.max(startDate1, startDate2).toDate()
-    : startDate1.toDate();
+    : startDate1?.toDate();
   const endDate = moment().utc().toDate();
 
   return [startDate, endDate];
