66a94e21c967fd3a3462d6dfdd20aadda23abcbd
danielailie12@gmail.com <danielailie12@gmail.com>
2023-05-08 19:44:50 +0300

    Analytics

---
+++ src/modules/rabbitmq/blockchain-events/analytics-events.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { AnalyticsService } from 'src/modules/analytics/analytics.service';
+import { DateUtils } from 'src/utils/date-utils';
+
+@Injectable()
+export class AnalyticsEventsService {
+  private readonly logger = new Logger(AnalyticsEventsService.name);
+
+  constructor(private analyticsService: AnalyticsService) {}
+
+  public async handleBuyEvents(auctionEvents: any[]) {
+    this.analyticsService.processEvents(
+      auctionEvents,
+      DateUtils.getCurrentTimestamp(),
+      DateUtils.getCurrentTimestamp(),
+    );
+  }
+}
