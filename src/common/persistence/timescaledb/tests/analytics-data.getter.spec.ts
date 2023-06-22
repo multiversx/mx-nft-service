ee8935839502ff14710ca729061eda19bd164e3f
danielailie <danielailie12@gmail.com>
2023-06-22 16:39:17 +0300

    Add analytics data getter tests

---
+++ src/common/persistence/timescaledb/tests/analytics-data.getter.spec.ts
@@ -0,0 +1,246 @@
+import { Test } from '@nestjs/testing';
+import { AnalyticsDataGetterService } from '../analytics-data.getter.service';
+import { FloorPriceDaily, SumDaily } from '../entities/sum-daily.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { AnalyticsAggregateValue } from 'src/modules/analytics/models/analytics-aggregate-value';
+
+describe('Analytics Data Getter Service', () => {
+  let service: AnalyticsDataGetterService;
+  let floorPriceRepository: any;
+  let sumDailyRepository: any;
+  const offset = jest.fn().mockReturnThis();
+  const limit = jest.fn().mockReturnThis();
+  const select = jest.fn().mockReturnThis();
+  const addSelect = jest.fn().mockReturnThis();
+  const andWhere = jest.fn().mockReturnThis();
+  const where = jest.fn().mockReturnThis();
+  const orderBy = jest.fn().mockReturnThis();
+  const groupBy = jest.fn().mockReturnThis();
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        AnalyticsDataGetterService,
+        {
+          provide: getRepositoryToken(SumDaily, 'timescaledb'),
+          useFactory: () => ({
+            save: jest.fn(() => true),
+          }),
+        },
+        {
+          provide: getRepositoryToken(FloorPriceDaily, 'timescaledb'),
+          useFactory: () => ({
+            save: jest.fn(() => true),
+          }),
+        },
+      ],
+    }).compile();
+
+    service = module.get<AnalyticsDataGetterService>(
+      AnalyticsDataGetterService,
+    );
+    floorPriceRepository = module.get(
+      getRepositoryToken(FloorPriceDaily, 'timescaledb'),
+    );
+    sumDailyRepository = module.get(
+      getRepositoryToken(SumDaily, 'timescaledb'),
+    );
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+  describe('getTopCollectionsDaily', () => {
+    it('returns empty list when no data and no key present', async () => {
+      const getRawMany = jest.fn().mockReturnValueOnce([]);
+      const getCount = jest.fn().mockReturnValueOnce(0);
+      sumDailyRepository.createQueryBuilder = jest.fn(() => ({
+        select,
+        offset,
+        limit,
+        getRawMany,
+        addSelect,
+        andWhere,
+        orderBy,
+        groupBy,
+        getCount,
+      }));
+      const result = await service.getTopCollectionsDaily({ metric: 'test' });
+      const expectedResult = [[], 0];
+
+      expect(result).toMatchObject(expectedResult);
+    });
+
+    it('returns list with one item when no data and the searched key', async () => {
+      const getRawMany = jest.fn().mockReturnValueOnce([]);
+      const getCount = jest.fn().mockReturnValueOnce(0);
+      sumDailyRepository.createQueryBuilder = jest.fn(() => ({
+        select,
+        offset,
+        limit,
+        getRawMany,
+        addSelect,
+        andWhere,
+        orderBy,
+        groupBy,
+        getCount,
+      }));
+      const result = await service.getTopCollectionsDaily({
+        metric: 'test',
+        series: 'test',
+      });
+      const expectedResult = [
+        [new AnalyticsAggregateValue({ value: 0, series: 'test' })],
+        1,
+      ];
+
+      expect(result).toMatchObject(expectedResult);
+    });
+
+    it('returns list with multiple items and expected values', async () => {
+      const getRawMany = jest.fn().mockReturnValueOnce([
+        { value: 2, series: 'test' },
+        { value: 121, series: 'test2' },
+      ]);
+      const getCount = jest.fn().mockReturnValueOnce(2);
+      sumDailyRepository.createQueryBuilder = jest.fn(() => ({
+        select,
+        offset,
+        limit,
+        getRawMany,
+        addSelect,
+        andWhere,
+        where,
+        orderBy,
+        groupBy,
+        getCount,
+      }));
+
+      const [responseList, responseCount] =
+        await service.getTopCollectionsDaily({
+          metric: 'test',
+        });
+
+      const [expectedList, expectedCount] = [
+        [
+          new AnalyticsAggregateValue({ value: 2, series: 'test' }),
+          new AnalyticsAggregateValue({ value: 121, series: 'test2' }),
+        ],
+        2,
+      ];
+
+      expect(responseCount).toBe(expectedCount);
+      expect(responseList).toMatchObject(expectedList);
+    });
+  });
+
+  describe('getFloorPriceData', () => {
+    it('returns empty list when no data for specific collection', async () => {
+      const getRawMany = jest.fn().mockReturnValueOnce([]);
+      floorPriceRepository.createQueryBuilder = jest.fn(() => ({
+        select,
+        offset,
+        limit,
+        getRawMany,
+        addSelect,
+        andWhere,
+        where,
+        orderBy,
+        groupBy,
+      }));
+      const result = await service.getFloorPriceData({
+        time: '10d',
+        metric: 'test',
+        series: 'test',
+      });
+      const expectedResult = [];
+
+      expect(result).toMatchObject(expectedResult);
+    });
+
+    it('returns list with multiple items and expected values', async () => {
+      const getRawMany = jest.fn().mockReturnValueOnce([
+        { value: 2, series: 'test' },
+        { value: 121, series: 'test' },
+      ]);
+      const expectedResult = [
+        new AnalyticsAggregateValue({ value: 2, series: 'test' }),
+        new AnalyticsAggregateValue({ value: 121, series: 'test' }),
+      ];
+      floorPriceRepository.createQueryBuilder = jest.fn(() => ({
+        select,
+        offset,
+        limit,
+        getRawMany,
+        addSelect,
+        where,
+        andWhere,
+        orderBy,
+        groupBy,
+      }));
+
+      const response = await service.getFloorPriceData({
+        time: '10d',
+        metric: 'test',
+        series: 'test',
+      });
+
+      expect(response).toMatchObject(expectedResult);
+    });
+  });
+
+  describe('getVolumeData', () => {
+    it('returns empty list when no data for specific collection', async () => {
+      const getRawMany = jest.fn().mockReturnValueOnce([]);
+      sumDailyRepository.createQueryBuilder = jest.fn(() => ({
+        select,
+        offset,
+        limit,
+        getRawMany,
+        addSelect,
+        andWhere,
+        where,
+        orderBy,
+        groupBy,
+      }));
+      const result = await service.getVolumeData({
+        time: '10d',
+        metric: 'test',
+        series: 'test',
+      });
+      const expectedResult = [];
+
+      expect(result).toMatchObject(expectedResult);
+    });
+
+    it('returns list with multiple items and expected values', async () => {
+      const getRawMany = jest.fn().mockReturnValueOnce([
+        { value: 2, series: 'test' },
+        { value: 121, series: 'test' },
+      ]);
+      const expectedResult = [
+        new AnalyticsAggregateValue({ value: 2, series: 'test' }),
+        new AnalyticsAggregateValue({ value: 121, series: 'test' }),
+      ];
+      sumDailyRepository.createQueryBuilder = jest.fn(() => ({
+        select,
+        offset,
+        limit,
+        getRawMany,
+        addSelect,
+        where,
+        andWhere,
+        orderBy,
+        groupBy,
+      }));
+
+      const response = await service.getVolumeData({
+        time: '10d',
+        metric: 'test',
+        series: 'test',
+      });
+
+      expect(response).toMatchObject(expectedResult);
+    });
+  });
+});
