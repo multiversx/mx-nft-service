c7df274001fb17c44fdb1913649cb675d21299f9
danielailie <danielailie12@gmail.com>
2023-09-05 12:29:18 +0300

    Change in memory cache to redis

--- src/modules/minters/minters-caching.service.ts
+++ src/modules/minters/minters-caching.service.ts
@@ -1,19 +1,18 @@
 import { Injectable, Logger } from '@nestjs/common';
 import '../../utils/extensions';
-import { CachingService } from '@multiversx/sdk-nestjs';
+import { RedisCacheService } from '@multiversx/sdk-nestjs';
 import { CacheInfo } from 'src/common/services/caching/entities/cache.info';
 import { MinterEntity } from 'src/db/minters';
 
 @Injectable()
 export class MintersCachingService {
-  constructor(private cacheService: CachingService, private readonly logger: Logger) {}
+  constructor(private redisCacheService: RedisCacheService) {}
 
   public async getMinters(getMinters: () => any): Promise<MinterEntity[]> {
-    return await this.cacheService.getOrSetCache(CacheInfo.Minters.key, () => getMinters(), CacheInfo.Minters.ttl);
+    return await this.redisCacheService.getOrSet(CacheInfo.Minters.key, () => getMinters(), CacheInfo.Minters.ttl);
   }
 
   public async invalidateMinters() {
-    this.logger.log(`Deleting cache key(s) ${CacheInfo.Minters.key} `);
-    await this.cacheService.deleteInCache(CacheInfo.Minters.key);
+    await this.redisCacheService.delete(CacheInfo.Minters.key);
   }
 }
