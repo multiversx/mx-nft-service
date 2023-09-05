4da79b1ecb1e30ce1c8443e741cc5d72a80e4cc7
danielailie <danielailie12@gmail.com>
2023-09-05 12:15:00 +0300

    Extra logging

--- src/modules/minters/minters-caching.service.ts
+++ src/modules/minters/minters-caching.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import '../../utils/extensions';
 import { CachingService } from '@multiversx/sdk-nestjs';
 import { CacheInfo } from 'src/common/services/caching/entities/cache.info';
@@ -6,13 +6,14 @@ import { MinterEntity } from 'src/db/minters';
 
 @Injectable()
 export class MintersCachingService {
-  constructor(private cacheService: CachingService) {}
+  constructor(private cacheService: CachingService, private readonly logger: Logger) {}
 
   public async getMinters(getMinters: () => any): Promise<MinterEntity[]> {
     return await this.cacheService.getOrSetCache(CacheInfo.Minters.key, () => getMinters(), CacheInfo.Minters.ttl);
   }
 
   public async invalidateMinters() {
+    this.logger.log(`Deleting cache key(s) ${CacheInfo.Minters.key} `);
     await this.cacheService.deleteInCache(CacheInfo.Minters.key);
   }
 }
