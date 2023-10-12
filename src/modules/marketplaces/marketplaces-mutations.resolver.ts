9c09ec5506aa6be60f9e63d011a8862e0004917d
danielailie <danielailie12@gmail.com>
2023-10-12 15:50:56 +0300

    Add console logs

--- src/modules/marketplaces/marketplaces-mutations.resolver.ts
+++ src/modules/marketplaces/marketplaces-mutations.resolver.ts
@@ -21,18 +21,21 @@ export class MarketplacesMutationsResolver extends BaseResolver(Marketplace) {
   @Mutation(() => Boolean)
   @UseGuards(JwtOrNativeAuthGuard, GqlAdminAuthGuard)
   async whitelistCollectionOnMarketplace(@Args('input') input: WhitelistCollectionArgs): Promise<Boolean> {
+    console.log('Whitelist collection', { input });
     return this.marketplaceService.whitelistCollectionOnMarketplace(WhitelistCollectionRequest.fromArgs(input));
   }
 
   @Mutation(() => Boolean)
   @UseGuards(JwtOrNativeAuthGuard, GqlAdminAuthGuard)
   async updateMarketplace(@Args('input') input: UpdateMarketplaceArgs): Promise<Boolean> {
+    console.log('update marketplace', { input });
     return this.marketplaceService.updateMarketplace(UpdateMarketplaceRequest.fromArgs(input));
   }
 
   @Mutation(() => Boolean)
   @UseGuards(JwtOrNativeAuthGuard, GqlAdminAuthGuard)
   async whitelistMarketplace(@Args('input') input: WhitelistMarketplaceArgs): Promise<Boolean> {
+    console.log('whitelist marketplace', { input });
     return this.marketplaceService.whitelistMarketplace(WhitelistMarketplaceRequest.fromArgs(input));
   }
 }
