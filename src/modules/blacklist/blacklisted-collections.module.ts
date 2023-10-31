367fee870217dcd373b3f851be8246c67862502d
danielailie <danielailie12@gmail.com>
2023-10-31 15:48:35 +0200

    fix missing dependencies

--- src/modules/blacklist/blacklisted-collections.module.ts
+++ src/modules/blacklist/blacklisted-collections.module.ts
@@ -3,9 +3,10 @@ import { CacheEventsPublisherModule } from '../rabbitmq/cache-invalidation/cache
 import { BlacklistedCollectionsCachingService } from './blacklisted-collections.caching.service';
 import { BlacklistedCollectionsResolver } from './blacklisted-collections.resolver';
 import { BlacklistedCollectionsService } from './blacklisted-collections.service';
+import { PersistenceModule } from 'src/common/persistence/persistence.module';
 
 @Module({
-  imports: [CacheEventsPublisherModule],
+  imports: [CacheEventsPublisherModule, PersistenceModule],
   providers: [BlacklistedCollectionsService, BlacklistedCollectionsCachingService, BlacklistedCollectionsResolver],
   exports: [BlacklistedCollectionsService],
 })
