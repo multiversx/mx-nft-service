7dbe8541ba8a4024b56a394a72052967bdf2accb
danielailie <danielailie12@gmail.com>
2023-12-13 14:54:14 +0200

    Remove unused services

--- src/modules/proxy-deployer/proxy-deployer.module.ts
+++ src/modules/proxy-deployer/proxy-deployer.module.ts
@@ -1,17 +1,15 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { MxCommunicationModule } from 'src/common';
 import { ProxyDeployerMutationsResolver } from './proxy-deployer-mutations.resolver';
-import { MintersService } from './proxy-deployer.service';
 import { PubSubListenerModule } from 'src/pubsub/pub.sub.listener.module';
 import { CommonModule } from 'src/common.module';
 import { AuthModule } from '../auth/auth.module';
-import { MintersCachingService } from './proxy-deployer-caching.service';
 import { ProxyDeployerAbiService } from './proxy-deployer.abi.service';
 import { CacheEventsPublisherModule } from '../rabbitmq/cache-invalidation/cache-invalidation-publisher/change-events-publisher.module';
 
 @Module({
-  providers: [ProxyDeployerMutationsResolver, MintersService, MintersCachingService, ProxyDeployerAbiService],
+  providers: [ProxyDeployerMutationsResolver, ProxyDeployerAbiService],
   imports: [PubSubListenerModule, MxCommunicationModule, CommonModule, forwardRef(() => AuthModule), CacheEventsPublisherModule],
-  exports: [MintersService, MintersCachingService],
+  exports: [],
 })
 export class ProxyDeployerModuleGraph {}
