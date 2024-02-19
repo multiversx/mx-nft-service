684f9b7da29b6bb462cbfee7c68353171c394506
Ramona Lobont <ramona.lobont@multiversx.com>
2024-02-19 11:13:16 +0200

    orm

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -6,21 +6,22 @@ const db: MysqlConnectionCredentialsOptions = {
   port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.NODE_ENV === 'test-e2e' ? ":memory:" : process.env.DB_NAME,
 };
 
 const dbSlaves = process.env.DB_SLAVES ? JSON.parse(process.env.DB_SLAVES) : [db];
 
 const config: ConnectionOptions = {
-  type: 'mysql',
-  synchronize: false,
-  dropSchema: false,
-  replication: {
-    master: db,
-    slaves: dbSlaves,
-  },
+  type:  process.env.NODE_ENV === 'test-e2e' ? 'sqlite' : 'mysql',
+  synchronize:  process.env.NODE_ENV === 'test-e2e' ? false : false,
+  dropSchema: process.env.NODE_ENV === 'test-e2e' ?  true : false,
+  // replication: {
+  //   master: db,
+  //   slaves: dbSlaves,
+  // },
+  database: process.env.NODE_ENV === 'test-e2e' ? ":memory:" : process.env.DB_NAME,
   entities: process.env.NODE_ENV === 'test-e2e' ? ['src/db/**/*.entity.js'] : ['dist/db/**/*.entity.js'],
-  migrations: ['dist/db/migrations/*.js'],
+  migrations: process.env.NODE_ENV === 'test-e2e' ? ['src/db/migrations/*.js'] : ['dist/db/migrations/*.js'],
   extra: {
     connectionLimit: process.env.DB_CONNECTION_LIMIT,
     ssl: {
