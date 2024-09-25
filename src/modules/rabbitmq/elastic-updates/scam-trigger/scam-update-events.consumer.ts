b3189d9d386d08fdf0d8a35c2a362a806f9c57bf
danielailie <danielailie12@gmail.com>
2024-09-25 14:43:42 +0300

    uncomment code for trigger scam

--- src/modules/rabbitmq/elastic-updates/scam-trigger/scam-update-events.consumer.ts
+++ src/modules/rabbitmq/elastic-updates/scam-trigger/scam-update-events.consumer.ts
@@ -3,6 +3,7 @@ import { rabbitExchanges, rabbitQueues } from '../../rabbit-config';
 import { MarkScamCollectionEvent } from './markScamCollection.event';
 import { CompetingRabbitConsumer } from '../../rabbitmq.consumers';
 import { CollectionScamService } from 'src/modules/scam/collection-scam.service';
+import { ScamInputEnum } from 'src/modules/admins/models/scam-update.input';
 @Injectable()
 export class ScamUpdateEventsConsumer {
   constructor(private readonly collectionScamService: CollectionScamService) { }
@@ -14,10 +15,10 @@ export class ScamUpdateEventsConsumer {
   })
   async consumeScamEvents(event: MarkScamCollectionEvent) {
     console.log({ event })
-    // if (event.type === ScamInfoTypeEnum.none) {
-    //   await this.collectionScamService.manuallyClearCollectionScamInfo(event.collectionIdentifier)
-    // } else {
-    //   await this.collectionScamService.manuallySetCollectionScamInfo(event.collectionIdentifier)
-    // }
+    if (event.type === ScamInputEnum.allow) {
+      await this.collectionScamService.manuallyClearCollectionScamInfo(event.collectionIdentifier)
+    } else {
+      await this.collectionScamService.manuallySetCollectionScamInfo(event.collectionIdentifier)
+    }
   }
 }
