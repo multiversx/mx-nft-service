56c01ac66f0448f4b2bc8b04e0abbc8aae221b29
danielailie <danielailie12@gmail.com>
2025-06-11 14:48:15 +0300

    remove region

--- src/modules/s3/s3.service.ts
+++ src/modules/s3/s3.service.ts
@@ -51,7 +51,6 @@ export class S3Service {
 
   getS3() {
     return new S3Client({
-      region: 'us-east-1',
       credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
