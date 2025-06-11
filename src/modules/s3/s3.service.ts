d742d4fc287150e8aa66928acb5262d823f4f6a5
danielailie <danielailie12@gmail.com>
2025-06-11 15:58:39 +0300

    Add s3 region

--- src/modules/s3/s3.service.ts
+++ src/modules/s3/s3.service.ts
@@ -51,6 +51,7 @@ export class S3Service {
 
   getS3() {
     return new S3Client({
+      region: process.env.AWS_S3_REGION,
       credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
